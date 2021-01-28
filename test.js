ce5cfbace6f8351574a1a67d7045fae497e3042a
Yang Xu <xydotguy@gmail.com>
2021-01-28 16:32:33 -0500

    first commit

--- test.js
+++
@@ -1,9 +0,0 @@
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach(box => {
-
-    box.addEventListener('keyup', (e) => {
-        console.log(e.key, document.activeElement.id);
-    });
-});
-
